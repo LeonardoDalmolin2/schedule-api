fe656cc92a97e9a88efd41857eae0551aff7fcf5
Leo <leonardodalmolin.ld@gmail.com>
2026-02-03 19:29:21 -0300

    chore: add simple request logger middleware

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,11 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, _res, next) => {
+  console.log(`[${req.method}] ${req.url}`);
+  next();
+});
+
 app.get('/api/health', (_req, res) => {
   res.status(200).json({ status: 'ok' });
 });
